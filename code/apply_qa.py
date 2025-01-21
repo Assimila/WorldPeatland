b09b1f2f40edc90d386e1106c93e7c92d7418d09
samdoolin <sam.doolin@assimila.eu>
2025-01-21 13:33:53 +0000

    remove python path modifications and fix import for glob

--- code/apply_qa.py
+++ code/apply_qa.py
@@ -18,9 +18,6 @@ logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger(__name__)
 
 
-sys.path.append('/workspace/TATSSI/')
-sys.path.insert(0, '/workspace/WorldPeatland/code/')
-
 ''' 
 apply_qa its the 3rd code to run will apply the qa settings to the MODIS data time series generated in code 2 
 then it will INTERPOLATE the cleaned time series

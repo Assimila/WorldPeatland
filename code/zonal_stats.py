8779cd0bd77580327ec40336b0a8000909290a2d
yarasarrouh <yara.sarrouh@assimila.eu>
2025-03-03 15:01:35 +0000

    cleanup zonal stats

--- code/zonal_stats.py
+++ code/zonal_stats.py
@@ -3,16 +3,17 @@ from osgeo import gdal
 import pandas as pd
 import time
 from tqdm import tqdm
-import glob
+from glob import glob
 import sys
 import os
-import geopandas as gpd
 from datetime import datetime
 import logging
 import matplotlib
+
 matplotlib.use('nbAgg')
 from rasterstats import zonal_stats
 from WorldPeatland.code.download_modis import create_dir
+from WorldPeatland.code.gdal_sheep import _get_FillValue
 
 logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger(__name__)
@@ -24,13 +25,21 @@ def calc_zonal_stat(data_tif_path, shapefile_path):
     # Count the bands or the number of layers in the tiff file (timesteps)
     bands = list(range(opn.RasterCount))
 
+    # get fill value from opn
+    _FillValue = _get_FillValue(opn)
     # Create an empty list to store the zonal statistics data
     l = []
     # Loop over all the bands in the tif, extract the zonal stat of each band
     # then store it in a dictionary because there is more than 1 stat calculated per band
     for b in tqdm(bands):
         time.sleep(0.5)
-        z = zonal_stats(shapefile_path, data_tif_path, stats="min mean max median", band=b + 1)
+        z = zonal_stats(
+            shapefile_path,
+            data_tif_path,
+            stats="min mean max median",
+            band=b + 1,
+            nodata=_FillValue
+        )
         dict_ = z[0]
         l.append(dict_)
 
@@ -58,16 +67,14 @@ def get_dts(opn, bands):
         lyr = opn.GetRasterBand(i + 1)
         mtd = lyr.GetMetadata()
         time = mtd['time']
-        time = datetime.strptime(time, '%Y-%m-%dT00:00:00.000000000').strftime('%d-%m-%Y')
+        time = datetime.strptime(time, '%Y-%m-%dT%H:%M:%S.%f000').strftime('%d-%m-%Y')
         dts.append(time)
     return dts
 
 
 def main(site_directory, shapefile_path):
-
-    data_tif_list = glob.glob(site_directory + 'MODIS/timeSeries/*tif')
+    data_tif_list = glob(site_directory + 'MODIS/timeSeries/*tif')
     modis_timeSeries_path = site_directory + f'/MODIS/timeSeries/'
-    zonal_stats_dir = create_dir(modis_timeSeries_path, 'ZonalStats')
 
     for data_tif_path in data_tif_list:
         # get tif file name
@@ -84,7 +91,8 @@ def main(site_directory, shapefile_path):
         # create pkl_filename from tif_filename
         pkl_filename = tif_filename.replace('.tif', '.zonalStats.pkl')
         # save the raw zonal stat in a pickle file
-        output_path = os.path.join(zonal_stats_dir, pkl_filename)
+        output_path = create_dir(modis_timeSeries_path, 'ZonalStats')
+        output_path = os.path.join(output_path, pkl_filename)
         df.to_pickle(output_path)
         LOG.info(f'Zonal stats successfully saved here: {output_path}')
 

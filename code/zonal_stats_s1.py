c57078ff9c33f64c686360725b4a0a02398c55ce
yarasarrouh <yara.sarrouh@assimila.eu>
2025-05-02 11:36:23 +0000

    zonal stats for cross ratio

---
+++ code/zonal_stats_s1.py
@@ -0,0 +1,121 @@
+# Import libraries
+from osgeo import gdal
+import pandas as pd
+import time
+from tqdm import tqdm
+import xarray as xr
+from glob import glob
+import sys
+import os
+from datetime import datetime
+import logging
+import matplotlib
+
+matplotlib.use('nbAgg')
+from rasterstats import zonal_stats
+from WorldPeatland.code.download_modis import create_dir
+from WorldPeatland.code.gdal_sheep import get_NoDataValue
+from WorldPeatland.code.utils import proj4_extract
+from WorldPeatland.code.save_xarray_to_gtiff_old import save_xarray_old
+
+logging.basicConfig(level=logging.INFO)
+LOG = logging.getLogger(__name__)
+
+
+""" 
+Steps to do before running this:
+- Make sure that the shapefile has one geometry otherwise the zonal stats will be calculated over each geometry
+- the shapefile and the tif files needs to be in the same coordinate system, can use this command line:
+
+ogr2ogr -t_srs "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +R=6371007.181 +units=m +no_defs" <output shp> <input shp>
+
+"""
+
+
+def calc_zonal_stat(data_tif_path, shapefile_path):
+
+    # Open the data in the tif file with gdal
+    opn = gdal.Open(data_tif_path)
+    # Count the bands or the number of layers in the tiff file (timesteps)
+    bands = list(range(opn.RasterCount))
+
+    # get fill value from opn
+    # _FillValue = get_NoDataValue(opn)
+    # Create an empty list to store the zonal statistics data
+    l = []
+    # Loop over all the bands in the tif, extract the zonal stat of each band
+    # then store it in a dictionary because there is more than 1 stat calculated per band
+    for b in tqdm(bands):
+        time.sleep(0.5)
+        z = zonal_stats(
+            shapefile_path,
+            data_tif_path,
+            stats="min mean max median",
+            band=b + 1,
+            nodata=float('nan')
+        )
+        dict_ = z[0]
+        l.append(dict_)
+
+    # Create a dataframe of the zonal stat
+    lst_min = []
+    lst_max = []
+    lst_mean = []
+    lst_median = []
+    for i in range(len(l)):
+        lst_min.append(l[i]['min'])
+        lst_max.append(l[i]['max'])
+        lst_mean.append(l[i]['mean'])
+        lst_median.append(l[i]['median'])
+
+    df = pd.DataFrame({"min": lst_min, "max": lst_max,
+                       "mean": lst_mean, "median": lst_median})
+
+    LOG.info(f'ZonalStats successfully calculated for {data_tif_path}')
+    return opn, bands, df
+
+
+def get_dts(opn, bands):
+    dts = []
+    for i in range(len(bands)):
+        lyr = opn.GetRasterBand(i + 1)
+        mtd = lyr.GetMetadata()
+        time = mtd['time']
+        time = datetime.strptime(time, '%Y-%m-%dT%H:%M:%S.%f000').strftime('%d-%m-%Y')
+        dts.append(time)
+    return dts
+
+
+def main(data_tif_path, shapefile_path):
+    # get tif file name
+    tif_filename = os.path.basename(data_tif_path)
+    LOG.info(f'ZonalStats calculation started for {tif_filename}')
+    opn, bands, df = calc_zonal_stat(data_tif_path, shapefile_path)
+
+    dts = get_dts(opn, bands)
+    # set the dts as index of the dataframe
+    df['Dates'] = dts
+    df['Dates'] = pd.to_datetime(df['Dates'], format='%d-%m-%Y')
+    df.set_index('Dates', inplace=True)
+
+    # create pkl_filename from tif_filename
+    pkl_filename = tif_filename.replace('.tif', '').replace('.', '_') + '.zonalStats.pkl'
+
+    directory = os.path.dirname(data_tif_path)
+
+    # save the raw zonal stat in a pickle file
+    output_path = create_dir(directory, 'ZonalStats')
+    output_path = os.path.join(output_path, pkl_filename)
+    df.to_pickle(output_path)
+    LOG.info(f'Zonal stats successfully saved here: {output_path}')
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 3:
+
+        print("Usage: python script.py <data_tif_path> <shapefile_path>")  # the user has to input one argument
+    else:
+        data_tif_path = sys.argv[1]
+        shapefile_path = sys.argv[2]
+        main(data_tif_path, shapefile_path)

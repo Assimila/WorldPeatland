a6a378e0ff9e5e08d8bd5f955b1d082099a11edf
yarasarrouh <yara.sarrouh@assimila.eu>
2025-08-22 09:16:01 +0000

    create functions, tidy up, reproject to one specific tif

--- code/s2_pre_processing.py
+++ code/s2_pre_processing.py
@@ -2,10 +2,13 @@ import os.path
 from glob import glob
 from datetime import datetime as dt
 import rasterio
+import dask.array as da
+import rioxarray
 import sys
 import numpy as np
 from osgeo import gdal
 import logging
+import gc
 
 from WorldPeatland.code.gdal_sheep import (gdal_dt, create_xarr, reproject_image,
                                            get_proj4_from_tif)
@@ -18,12 +21,15 @@ logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger(__name__)
 
 
-def get_bands_SR_in_arrays(bd, tif, opn, month_tif, input_dict):
+def get_bands_SR_in_arrays(i, tif, target_b02_tif, input_dict):
     """
     1. Get the surface reflectance bands (other than B2 SR)
     2. regrid to 10 m pixel resolution
     3. put the surface reflectance in arrays + multiply by scaling factor
     4. add to input_dict
+
+    INPUT
+     - i (int) - index number to know which band is refered to
 
     OUTPUT
         - input_dict (dictionary) - populated input_dict with all surface reflectance bands needs for MLEONN
@@ -31,52 +37,80 @@ def get_bands_SR_in_arrays(bd, tif, opn, month_tif, input_dict):
 
     # get name of the spectral band from the tif path
     band_name = tif.split('/')[-2]
-
-    # Check that all monthly tif files for all the reflectance have the same no. of timesteps as B02
-    with rasterio.open(tif) as src:
-        if src.count != opn.RasterCount:
-            raise ValueError(f'Raster count for {tif} does not match B02 timesteps'
-                             f'Expected: {opn.RasterCount}, Found: {src.count}')
-
     # regrid the tifs for the same B02 pixel size
-    g = reproject_image(tif, month_tif)
+    g = reproject_image(tif, target_b02_tif)
     # g is a gdal.dataset, pick the same raster band number in the loop
-    rb_g = g.GetRasterBand(bd)
-    arr_g = rb_g.ReadAsArray()
-    SR_BD_SCALE = 10000
-    input_dict[f'{band_name}'] = (arr_g / SR_BD_SCALE).astype(np.float32)
+    arr = __get_SR_arr_from_band_no(g, i)
+    input_dict[f'{band_name}'] = arr
 
     return input_dict
 
 
-def prepare_run_MLEONN(bd, opn, month_tif, reflectance_tifs):
-    """
-    Prepare the input bands surface reflectance + Run MLEONN
-    OUTPUT
-        - MLEONN_products_dict (dictionary) - dict with keys as MLEONN products name and with
-            corresponding values as list of arrays
-    """
-    # Start by adding the reflectance B02 array to the input_dictionary
-    rb = opn.GetRasterBand(bd)
-    # Read the band 2d array
-    arr = rb.ReadAsArray()
-    # multiply by scaling factor
-    SR_BD_SCALE = 10000
-    arr = (arr / SR_BD_SCALE).astype(np.float32)
-    # create a dictionary where you will append all the 2d arrays needed as inputs for MLEONN
-    input_dict = {'B02': arr}
-    # Now loop over all other bands
-    for tif in reflectance_tifs:
-        # Populate the input_dict with descaled data from the other bands (other than B2)
-        input_dict = get_bands_SR_in_arrays(bd, tif, opn, month_tif, input_dict)
+def __get_in_band_mtd(i, opn):
+    """get the time metadata from the surface reflectance tif for the band with index i"""
 
     # Get the in band metadata information from the B02 monthly tif file
+    rb = opn.GetRasterBand(i)
     mtd = rb.GetMetadata()  # mtd is a dictionary containing all in band metadata
-
     # Get the timestep from the metadata to reconstruct the monthly tif of the MLEONN products
     time_str = mtd['time']
     dt_format = '%Y-%m-%d %H:%M:%S'
-    time = dt.strptime(time_str, dt_format)
+    return dt.strptime(time_str, dt_format)
+
+
+def __get_SR_arr_from_band_no(input_ds, i=None, scale=10000):
+    """
+    Extract a surface reflectance band as a scaled NumPy array.
+
+    Parameters
+    ----------
+    input_ds : str or gdal.Dataset
+        Path to a raster file (GeoTIFF, etc.) or an already opened GDAL dataset.
+    i : int, optional
+        Band number to read (1-based indexing). Defaults to 1 if None.
+    scale : float, optional
+        Scaling factor (default 10000.0 for Sentinel-2 surface reflectance).
+
+    Returns
+    -------
+    np.ndarray
+        2D NumPy array of reflectance values (float32), scaled to [0, 1+].
+    """
+    # Open dataset if given a path
+    if isinstance(input_ds, str):
+        ds = gdal.Open(input_ds)
+        if ds is None:
+            raise ValueError(f"Could not open file: {input_ds}")
+    else:
+        ds = input_ds
+
+    # Default band index
+    if i is None:
+        i = 1
+
+    # Extract band
+    rb = ds.GetRasterBand(i)
+    arr = rb.ReadAsArray()
+
+    # Apply scaling
+    return (arr / scale).astype(np.float32)
+
+
+def __get_target_arr_shape(tif):
+    """Get the shape of one band in the target tif 2d shape"""
+
+    ds = gdal.Open(tif)
+    # gdal object indexing starts with 1 not 0
+    band1 = ds.GetRasterBand(1).ReadAsArray()
+    return band1.shape
+
+
+def __get_angular_mtd(i, opn):
+    """get the angular values in float 32 from the in band metadata for the band number i"""
+
+    rb = opn.GetRasterBand(i)
+    # Get the in band metadata information from the B02 monthly tif file
+    mtd = rb.GetMetadata()  # mtd is a dictionary containing all in band metadata
 
     # extract the angular information as numpy float 32 (it is set as str)
     vaa = np.float32(mtd['vaa'])
@@ -84,11 +118,35 @@ def prepare_run_MLEONN(bd, opn, month_tif, reflectance_tifs):
     saa = np.float32(mtd['saa'])
     sza = np.float32(mtd['sza'])
 
-    # form a numpy array for each angle same shape as the B02 reflectance array
-    vaa = np.full(arr.shape, vaa)
-    vza = np.full(arr.shape, vza)
-    saa = np.full(arr.shape, saa)
-    sza = np.full(arr.shape, sza)
+    return vaa, vza, saa, sza
+
+
+def prepare_run_MLEONN(bd_no, op_b02_monthly, target_b02_tif, reflectance_tifs, target_shape):
+    """
+    Prepare the input bands surface reflectance + Run MLEONN
+    INPUTS:
+        - bd_no (int) - band number it would mean which timestep to refer to for all SR
+        - op
+        - target_b02_tif (str) path to a tif B02 SR
+    OUTPUT
+        - MLEONN_products_dict (dictionary) - dict with keys as MLEONN products name and with
+            corresponding values as list of arrays
+    """
+    arr_b02 = __get_SR_arr_from_band_no(op_b02_monthly, bd_no)
+    # create a dictionary where you will append all the 2d arrays needed as inputs for MLEONN
+    input_dict = {'B02': arr_b02}
+
+    # Now loop over all other SR
+    for tif in reflectance_tifs:
+        # Populate the input_dict with descaled data from the other bands (other than B2)
+        input_dict = get_bands_SR_in_arrays(bd_no, tif, target_b02_tif, input_dict)
+
+    vaa, vza, saa, sza = __get_angular_mtd(bd_no, op_b02_monthly)
+    # form a numpy array for each angle same shape as the target B02 reflectance array
+    vaa = np.full(target_shape.shape, vaa)
+    vza = np.full(target_shape.shape, vza)
+    saa = np.full(target_shape.shape, saa)
+    sza = np.full(target_shape.shape, sza)
 
     # add the arrays to the input_dict
     input_dict['vaa'] = vaa
@@ -104,20 +162,20 @@ def prepare_run_MLEONN(bd, opn, month_tif, reflectance_tifs):
 
     # Empty the input_dict
     input_dict = None
-    LOG.info(f'MLEONN four products successfully formed for {time}')
 
-    return lai, time # fapar, fc, cab,
+    LOG.info(f'prepare and run MLEONN done for band number {bd_no}')
+    return lai  #, fapar, fc, cab,
 
 
-def create(var_arr, cloud_mask, dts, opn, varname, proj4_string, path, timestep):
+def create(arr, cloud_mask, list_datetime, op, varname, proj4_string, path, timestep):
     """
     Processes and saves geospatial data with cloud masking applied.
 
     Parameters:
-        var_arr (numpy.ndarray): Input data array for the variable (e.g., LAI, fapar).
+        arr (numpy.ndarray): Input data array for the variable (e.g., LAI, fapar).
         cloud_mask (numpy.ndarray): Boolean array indicating cloud-covered areas (True = cloud).
-        dts (datetime): Timestamps associated with the data.
-        opn (dict): Operational metadata or parameters.
+        list_datetime (list of datetimes): Timestamps associated with the data.
+        op (dict): Operational metadata or parameters.
         varname (str): Name of the variable being processed.
         proj4_string (str): CRS in Proj4 format.
         path (str): Directory path to save the output files.
@@ -128,10 +186,10 @@ def create(var_arr, cloud_mask, dts, opn, varname, proj4_string, path, timestep)
     """
     try:
         # Apply cloud mask to the data array
-        masked_arr = np.where(~cloud_mask, var_arr, np.nan)
+        masked_arr = np.where(~cloud_mask, arr, np.nan)
 
         # Create an xarray object
-        masked_xr = create_xarr(opn, varname, masked_arr, dts)
+        masked_xr = create_xarr(op, varname, masked_arr, list_datetime)
         masked_xr.attrs['crs'] = proj4_string
 
         # Create output directory
@@ -142,12 +200,14 @@ def create(var_arr, cloud_mask, dts, opn, varname, proj4_string, path, timestep)
         fname = os.path.join(output_dir, f"{varname}_{timestep}.tif")
         save_xarray_old(fname, masked_xr, varname)
 
+        del masked_xr
+        gc.collect()
+
     except Exception as e:
         LOG.error(f"Error in creating file for {varname}: {e}")
 
 
 def main(site_dir):
-
     # Check if site_dir exist
     if os.path.isdir(site_dir):
         LOG.info(f"The directory '{site_dir}' exists.")
@@ -163,27 +223,32 @@ def main(site_dir):
     B02_tif_files_list = sorted(glob(pattern))
 
     # Set reflectance bands list
-    bands = ['B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B8A', 'B11', 'B12']
+    SR_bands = ['B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B8A', 'B11', 'B12']
 
-    for month_tif in B02_tif_files_list:
+    # TODO: pick the target file manually that have the crs and geotransform you want to unify for all other tifs
+    target_b02_tif = B02_tif_files_list[1]
+    target_shape = __get_SR_arr_from_band_no(target_b02_tif, i=None)
+    target_proj4_string = get_proj4_from_tif(target_b02_tif)
+
+    for month_b02_tif in B02_tif_files_list[14:]:  # TODO removed [60:]
         # extract timestep from the filename
-        timestep = get_timestep_from_tif(month_tif)
+        timestep = get_timestep_from_tif(month_b02_tif)
         LOG.info(f'Starting processing this timestep: {timestep}')
         # list of all SR bands tiffs for the same month
         reflectance_tifs = []
-        for band in bands:
-            pattern = os.path.join(s2_path, band, f'*{timestep}*.tif')
+        for SR_band in SR_bands:
+            pattern = os.path.join(s2_path, SR_band, f'*{timestep}*.tif')
             reflectance_tifs.extend(glob(pattern, recursive=True))
 
         # Open the monthly B02 tif file
-        opn = gdal.Open(month_tif)
+        opn_monthly_b02 = gdal.Open(month_b02_tif)
 
         # Check that all monthly tif files for all the reflectance have the same no. of timesteps as B02
         for tif in reflectance_tifs:
             with rasterio.open(tif) as src:
-                if src.count != opn.RasterCount:
+                if src.count != opn_monthly_b02.RasterCount:
                     raise ValueError(f'Raster count for {tif} does not match B02 timesteps'
-                                     f'Expected: {opn.RasterCount}, Found: {src.count}')
+                                     f'Expected: {opn_monthly_b02.RasterCount}, Found: {src.count}')
 
         # Datetime empty list
         dts = []
@@ -194,10 +259,14 @@ def main(site_dir):
 
         # Now loop over each timestep in this month, loop over each raster in the tif
         # in this case bd as band meaning a 2d raster or one timestep and not reflectance bands
-        for bd in range(1, opn.RasterCount + 1):  # gdal starts raster band count from 1
+        for band_no in range(1, opn_monthly_b02.RasterCount + 1):  # gdal starts raster band count from 1
             # PREPARE INPUT BAND SR AND RUN MLEONN
             LOG.info(f'MLEONN process starting for {timestep}')
-            lai, specific_date = prepare_run_MLEONN(bd, opn, month_tif, reflectance_tifs) #fapar, fc, cab,
+            lai = prepare_run_MLEONN(band_no, opn_monthly_b02, target_b02_tif, reflectance_tifs,
+                                     target_shape)  # fapar, fc, cab,
+            # Get the in band metadata information from the B02 monthly tif file
+            specific_date = __get_in_band_mtd(band_no, opn_monthly_b02)
+            LOG.info(f'MLEONN four products successfully formed for {specific_date}')
             dts.append(specific_date)
             lai_list.append(lai)
             # fapar_list.append(fapar)
@@ -217,7 +286,7 @@ def main(site_dir):
         pattern = os.path.join(site_dir, 'Sentinel', 'MSIL2A', 'datacube', 'S2_SR', 'SCL', f'*{timestep}.tif')
         fname = glob(pattern)[0]
         # Resample the SCL band (downscaling the SCL band pixel originally 60m to 20m)
-        SCL_gdalobj = reproject_image(fname, month_tif)
+        SCL_gdalobj = reproject_image(fname, target_b02_tif)
         SCL_resampled = SCL_gdalobj.ReadAsArray()  # array dtype=uint8
 
         # Create masks for each class based on SCL
@@ -231,19 +300,25 @@ def main(site_dir):
         # Get the numpy arrays of the xr.datasets to preserve shape
         # lai_array > 8.0 removing impossible values of LAI according to S2Toolbox Level2Products version 2.0
         # table 9.
+
+        # True or 1 is a masked pixel. False or 0 is not masked
         mask = (
                 is_cloud | is_shadow | is_snow | is_cirrus |
                 (lai_array > 8.0) | (lai_array < 0) | is_water
-        ) # dtype numpy array
-        # Construct file path and save
-        proj4_string = get_proj4_from_tif(month_tif)
+        )  # dtype numpy array
 
+        del is_water, is_snow, is_cirrus, is_cloud, is_shadow
+
+        # Construct file path and save
         path = os.path.join(site_dir, 'Sentinel', 'MSIL2A', 'datacube', 'MLEONN')
         output_dir = create_dir(path, 'mask')
         fname = os.path.join(output_dir, f"mask_{timestep}.tif")
-        ds_mask = create_xarr(opn, 'bool', mask, dts)
-        ds_mask.attrs['crs'] = proj4_string
+        opn_target = gdal.Open(target_b02_tif)
+        ds_mask = create_xarr(opn_target, 'bool', mask, dts)
+        ds_mask.attrs['crs'] = target_proj4_string
         save_xarray_old(fname, ds_mask, 'bool')
+        del ds_mask
+        gc.collect()
 
         LOG.info(f'Mask successfully formed & saved for {timestep}')
 
@@ -256,7 +331,10 @@ def main(site_dir):
 
         for varname, var_arr in variables.items():
             LOG.info(f"Processing {varname}...")
-            create(var_arr, mask, dts, opn, varname, proj4_string, path, timestep)
+            create(var_arr, mask, dts, opn_target, varname, target_proj4_string, path, timestep)
+
+        del variables, mask
+        gc.collect()
 
 
 if __name__ == "__main__":

93524a54e8b31eca3f3c514f43951ea468a3f3f4
yarasarrouh <yara.sarrouh@assimila.eu>
2025-08-27 10:36:12 +0000

    replace <0 with 0 for lai after interp/smoothn artefacts

--- code/sentinel_interp_smooth.py
+++ code/sentinel_interp_smooth.py
@@ -4,10 +4,10 @@ import xarray as xr
 import logging
 from glob import glob
 from dask.diagnostics import ProgressBar
-from WorldPeatland.code.utils import create_dir, proj4_extract
-from WorldPeatland.code.gdal_sheep import gdal_stack_dt, create_xarr
+from WorldPeatland.code.utils import create_dir
+from WorldPeatland.code.gdal_sheep import concat_xr, get_proj4_from_tif
 from WorldPeatland.code.save_xarray_to_gtiff_old import save_xarray_old
-from WorldPeatland.code.s1_pre_processing import process_and_save_block, DW_smoothn_smooth_xarray
+from WorldPeatland.code.s1_pre_processing import DW_smoothn_smooth_xarray
 
 LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.DEBUG)
@@ -17,17 +17,25 @@ def interpolate_block(block):
     """
     interpolate a single block and save it to disk.
     """
+    LOG.info(f"Linear interpolation started for the block")
     interpolated_block = block.interpolate_na(dim='time', method='linear')
     LOG.info(f'Linear interpolation successful')
     return interpolated_block
 
-def process_smoothn_block(block, var_name, window_size, flag):
+
+def process_and_save_block(block, save_path, var_name, window_size, flag):
     """
     Smooth a single block and save it to disk.
     """
+    LOG.info(f"Smoothn started for the block")
     smoothed_block = DW_smoothn_smooth_xarray(block, var_name, window_size, flag)
+
+    # replace negative values with 0
+    smoothed_block = smoothed_block.where(smoothed_block >= 0, 0)
+
     LOG.info(f'smoothn {window_size} successful')
-    return smoothed_block
+    smoothed_block.to_netcdf(save_path)
+    LOG.info(f'{save_path} successfully saved')
 
 def process_large_dask_chunks(dask_dataset, block_size, var_name, flag, output_dir):
     """
@@ -56,8 +64,6 @@ def process_large_dask_chunks(dask_dataset, block_size, var_name, flag, output_d
 
                 # 1st smoother
                 window_size1 = 1.5
-                block = process_smoothn_block(block, var_name, window_size1, flag)
-
                 block_path = f"{output_dir}/smoothed_block_{lat_start}_{lon_start}.linear.smoothn{window_size1}.nc"
                 process_and_save_block(block, block_path, var_name, window_size1, flag)
 
@@ -127,20 +133,15 @@ def main(site_dir):
         # get the tif files to interpolate
         tif_files = sorted(glob(os.path.join(p, "*.tif")))
         # Load and stack the files
-        stacked_arr, dts, saved_opn = gdal_stack_dt(tif_files)
-        data_with_nan = create_xarr(saved_opn, var_name, stacked_arr, dts)
+        dask_chunks = concat_xr(tif_files, var_name)
         # create directory to interpol
         interp_dir = create_dir(p, 'interpolated')
-        proj4_string = proj4_extract(saved_opn)
 
-        LOG.info('Dask chucking began')
-        dask_chunks = data_with_nan.chunk({"latitude": 5, "longitude": 5})
-
-        LOG.info('Begin block processing]')
+        LOG.info('Begin block processing')
         output_blocks_dir = create_dir(interp_dir, 'dask_processing_outputs')
         processed_result = process_large_dask_chunks(
             dask_dataset=dask_chunks,
-            block_size=100,  # Spatial block size
+            block_size=10,  # Spatial block size
             var_name=var_name,  # Variable to smooth
             flag=True,  # Smoothing flag
             output_dir=output_blocks_dir,
@@ -152,24 +153,24 @@ def main(site_dir):
         output_dir = os.path.join(interp_dir, fname)
 
         # set extract and set proj 4 to xarray
-        processed_result.attrs['crs'] = proj4_string
+        get_proj4_from_tif(tif_files[0], xarray=processed_result)
         save_xarray_old(output_dir, processed_result, var_name)
 
         # Detrend with a period of 73, check how many observations do we have per year
-        dask_chunks = processed_result.chunk({"time": -1, "latitude": 5, "longitude": 5})
+        dask_chunks = processed_result.chunk({"time": -1, "latitude": 10, "longitude": 10})
         period = 182
         output_blocks_dir = create_dir(interp_dir, 'dask_processing_outputs_dtr')
         ds_dtr = process_large_dask_chunks_2(
             dask_dataset=dask_chunks,
-            block_size=100,  # Spatial block size
+            block_size=10,  # Spatial block size
             var_name=var_name,  # Variable to smooth
             period=period,  # Smoothing flag
             output_dir=output_blocks_dir,
         )
 
-        LOG.info(f'Detrending mleon product')
+        LOG.info(f'Detrending MLEONN product')
         ds_dtr = ds_dtr.astype('float32')
-        ds_dtr.attrs['crs'] = proj4_string
+        get_proj4_from_tif(tif_files[0], xarray=ds_dtr)
 
         fname = f'{var_name}.linear.smoothn1.5.detrended.{period}.tif'
         output_dir = os.path.join(interp_dir, fname)
